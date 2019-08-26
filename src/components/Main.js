e7a2cec313381deadfca026cc3ca41df83b7cb41
Harpreetpreet <preetx025@gmail.com>
2019-08-26 14:58:56 +0530

    add-files

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,7 @@ margin-top:6rem;
 const Main = () => (
 <Container>
 <h2 className="column is-3 is-offset-5 has-text-weight-semibold">ABOUT OUR STUDIO</h2>
-<h1 className="column is-7 is-offset-2 is-size-3 has-text-weight-bold">
+<h1 className="has-text-centered is-size-3 has-text-weight-bold">
 THE CAMERA IS AN INSTRUMENT THAT TEACHES
 <h1 classname="column is- is-offset-3 is-size-3 has-text-weight-bold"> PEOPLE HOW TO SEE WITHOUT A CAMERA</h1>
 </h1>
