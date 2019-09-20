fac59e8d2e3f9d1319eb39116814c07d93c8617f
Harpreetpreet <preetx025@gmail.com>
2019-09-20 11:45:29 +0530

    add-button

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,45 +2,30 @@ import React from "react"
 import styled from "styled-components"
 
 const Container =styled.div`
-.text:hover {
-color: #FF4800;
-}
-.section{
-height:32rem;
-}
-.paragraph{
-    margin-top: 2rem;
-}
-.radius{
-    padding: 1rem;
-    border-radius: 37px;
-    background: #272727;
-    color:white;
-}
-.radius:hover{
-    background-color:#FF4800;
-    color:white;
-    }
+margin-top:5rem;
+ p{
+     margin:3rem;
+ }
+ .head{
+      margin-top:2rem;
+  }
 `
-const Main = () => (   
-    <Container>
-        <section className="section">
-            <container>
-<a>
-<h2 className="text has-text-centered has-text-weight-semibold has-text-black">ABOUT OUR STUDIO</h2>
-</a>
-<h1 className=" height  has-text-centered is-size-3 has-text-weight-bold">
-THE CAMERA IS AN INSTRUMENT THAT TEACHES
-<h1 className=" is-size-3 has-text-weight-bold"> PEOPLE HOW TO SEE WITHOUT A CAMERA</h1>
-</h1>
-<div className=" paragraph column is-8 is-offset-2 has-text-centered">
-<p>
-It’s the end of summer the sweltering heat makes human sweat in the night and makes the plants and trees wilt even in the moonlit nights. The eastern wind breeze brings an eerie feeling, that the monsoon clouds are soon coming, there is a strange silence in the ears, the sky gets darker and darker, the flash of lightning illuminates the dark skies, the sound of thunder fills the heart with fear.
-</p>
-<a className="radius has-text-weight-semibold has-text-centered">READ MORE</a>
-</div>
-</container>
-</section>
+const Main =() =>  (
+<Container>
+    <section className="section">
+        <div className="container">
+    <h3 className="has-text-centered has-text-weight-semibold has-text-grey">ABOUT OUR STUDIO</h3>
+    <span className=" is-size-3 has-text-black">
+    <h1 className="head has-text-centered  has-text-weight-bold">THE CAMERA IS AN INSTRUMENT THAT TEACHES</h1>
+    <h1 className="has-text-centered  has-text-weight-bold">PEOPLE HOW TO SEE WITHOUT A CAMERA</h1>
+    </span>
+    <p className="column is-8 is-offset-2 has-text-centered">It’s the end of summer the sweltering heat makes human sweat in the night and makes the plants and trees wilt even in the moonlit nights. The eastern wind breeze brings an eerie feeling, that the monsoon clouds are soon coming, there is a strange silence in the ears, the sky gets darker and darker, the flash of lightning illuminates the dark skies, the sound of thunder fills the heart with fear.</p>
+    <div className="buttons has-addons is-centered">
+    <a className="button is-primary is-rounded ">READ ME</a>
+    </div>
+    </div>
+    </section>
 </Container>
+
 );
 export default Main;
