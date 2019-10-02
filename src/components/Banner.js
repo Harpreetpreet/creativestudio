ee70920e65d9941f3fbcc1c1a77632f7be108af6
Harpreetpreet <preetx025@gmail.com>
2019-10-01 21:36:25 -0700

    add-image

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 const Container = styled.h2`
-    background: url("https://colorlib.com/preview/theme/shotgear/img/banner_bg.png");
+    background: url("/static/images/pic01.png");
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center; 
