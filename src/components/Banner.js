11834871f1b01a9e131033b524b46f51fdb47d4d
dharmveer bangar <dvbangar8@gmail.com>
2019-12-22 17:06:20 +0530

    clean up

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,42 +2,34 @@ import React from "react"
 
 import styled from "styled-components"
 
-const Container = styled.div`
+const Section = styled.div`
   background: url("/images/bgimage.jpg");
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
-  margin-top: 6rem;
-  height: 40rem;
 
-  .head {
-    padding-top: 18rem;
-  }
-  .color {
-    margin-left: 0.5rem;
-  }
-  .hint {
-    margin-right: 1rem;
-  }
 `
 const Banner = () => (
-  <Container>
-    <div>
-      <h2 className="head column is-3 is-offset-7 is-size-5 has-text-white has-text-centered has-text-weight-semibold">
-        MODEL PHOTOGRAPY
-      </h2>
-      <div className="column is-3 is-offset-7">
-        <h1 className=" hint has-text-white is-size-2 has-text-weight-bold  has-text-centered">
-          CREATIVE
-          <span className="color has-text-danger is-size-2 has-text-weight-bold has-text-centered">
-            STUDIO
-          </span>
-        </h1>
+  <Section className="hero section is-block is-relative is-white is-large">
+    <div className="container">
+      <div className="columns is-vcentered is-desktop">
+        <div className="column is-7" />
+        <div className="column is-5-desktop has-text-centered-touch">
+          <h2 className="is-size-5 has-text-white has-text-centered has-text-weight-semibold">
+            MODEL PHOTOGRAPY
+          </h2>
+          <h1 className="has-text-white is-size-2 has-text-weight-bold  has-text-centered">
+            CREATIVE
+            <span className="color has-text-danger is-size-2 has-text-weight-bold has-text-centered">
+              STUDIO
+            </span>
+          </h1>
+          <h3 className="has-text-centered has-text-white">
+            Capturing moments from today
+          </h3>
+        </div>
       </div>
-      <h3 className="item has-text-centered has-text-white column is-3 is-offset-7">
-        Capturing moments from today
-      </h3>
     </div>
-  </Container>
+  </Section>
 )
 export default Banner
